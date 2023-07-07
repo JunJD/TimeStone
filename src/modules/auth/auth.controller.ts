da2b88205e4795977790a403e5be70f98cf6627b
dingjunjie <dingjunjie@ypshengxian.com>
2023-07-07 16:56:20 +0800

    feat: 登录接口

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,13 +2,14 @@ import { Post, Controller, Body, HttpException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { User } from '../user/common/entity/user.entity';
+import { LoginAuthDto } from './dto/login-auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly userService: UserService) {}
 
   @Post('register')
-  async signup(@Body() createUserDto: CreateUserDto): Promise<User> {
+  async signUp(@Body() createUserDto: CreateUserDto): Promise<User> {
     const user = await this.userService.findOne(
       createUserDto as Pick<CreateUserDto, 'email'>,
     );
@@ -16,5 +17,17 @@ export class AuthController {
       throw new HttpException('用户已存在', 401);
     }
     return this.userService.createUser(createUserDto);
+  }
+
+  @Post('login')
+  async signIn(@Body() loginAuthDto: LoginAuthDto): Promise<User> {
+    const user = await this.userService.findOne(
+      loginAuthDto as Pick<LoginAuthDto, 'email'>,
+    );
+    if (!user) {
+      throw new HttpException('用户不存在', 401);
+    }
+    return user;
+    // return this.userService.createUser(createUserDto);
   }
 }
