2ecf5973ed731d672f4701b9fb7443114447c0cd
dingjunjie <dingjunjie@ypshengxian.com>
2023-07-11 13:03:15 +0800

    feat: 创建用户

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,6 @@
-import {
-  Post,
-  Controller,
-  Body,
-  HttpException,
-  HttpStatus,
-  HttpCode,
-} from '@nestjs/common';
+import { Post, Controller, Body, HttpStatus, HttpCode } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
-import { User } from '../user/common/entity/user.entity';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { AuthService } from './auth.service';
 import { Public } from './decorators/public.decorator';
@@ -23,14 +15,8 @@ export class AuthController {
   @Public()
   @HttpCode(HttpStatus.OK)
   @Post('register')
-  async signUp(@Body() createUserDto: CreateUserDto): Promise<User> {
-    const user = await this.userService.findOne(
-      createUserDto as Pick<CreateUserDto, 'email'>,
-    );
-    if (user) {
-      throw new HttpException('用户已存在', 401);
-    }
-    return this.userService.createUser(createUserDto);
+  async signUp(@Body() createUserDto: CreateUserDto) {
+    return this.authService.signUp(createUserDto);
   }
 
   @Public()
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { LoginAuthDto } from './dto/login-auth.dto';
 
 import { HttpException, Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import { CreateUserDto } from '../user/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -10,6 +11,24 @@ export class AuthService {
     private userService: UserService,
     private jwtService: JwtService,
   ) {}
+
+  // 注册依然返回token
+  async signUp(createUserDto: CreateUserDto) {
+    const _user = await this.userService.findOne(
+      createUserDto as Pick<CreateUserDto, 'email'>,
+    );
+    if (_user) {
+      throw new HttpException('用户已存在', 401);
+    }
+    const user = await this.userService.createUser(createUserDto);
+
+    const payload = { username: user?.email, sub: user?.name };
+
+    return {
+      ...payload,
+      accessToken: await this.jwtService.signAsync(payload),
+    };
+  }
 
   // 登录 生成token
   async signIn(loginAuthDto: LoginAuthDto): Promise<{ accessToken: string }> {
@@ -20,8 +39,6 @@ export class AuthService {
     if (user === null) {
       throw new HttpException('用户不存在', 401);
     }
-
-    console.log(loginAuthDto.password, user);
 
     if (user?.password !== loginAuthDto.password) {
       throw new HttpException('密码错误', 401);
